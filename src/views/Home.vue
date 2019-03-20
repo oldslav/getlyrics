5bdd26894110f4af72e8bc65d0e7a16a323c6750
Slava Afanasev <vyaches.afanas@gmail.com>
2019-03-20 10:37:51 +0400

    markup fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,19 @@
 <template>
   <v-container grid-list-xl>
     <v-layout row wrap align-center justify-center>
-      <v-flex xl3 xs12>
+      <v-flex xl3 lg3 xs12>
         <v-text-field v-model="payload.band" label="Band" placeholder="Band"></v-text-field>
       </v-flex>
-      <v-flex xl3 xs12>
+      <v-flex xl3 lg3 xs12>
         <v-text-field v-model="payload.title" label="Title" placeholder="Title"></v-text-field>
       </v-flex>
       <v-btn color="success" @click="getLyrics">Find lyrics</v-btn>
     </v-layout>
     <v-layout row wrap align-center justify-center>
-      <v-flex xl6 sm12>
-        <v-card v-show="lyrics">
+      <v-flex xl6 lg6 sm12 text-xs-center>
+        <v-card v-show="lyrics" elevation="12">
           <v-card-text>
-            <p style="white-space: pre-line">{{ lyrics }}</p>
+            <h3 style="white-space: pre-line">{{ lyrics }}</h3>
           </v-card-text>
         </v-card>
       </v-flex>
